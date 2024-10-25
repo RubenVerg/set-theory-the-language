ab4c51b90c7bf4286323f2719b14482f9fc0dd25
RubenVerg <RubenVerg@outlook.com>
2024-10-25 16:15:29 +0200

    Janky input and outputs

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,65 @@
 module Main where
 
+import Language.STTL.Set
+import Language.STTL.Parser
 import Language.STTL.Interpreter
 import Language.STTL.Context
+import Language.STTL.Multiverse
+import Language.STTL.DefaultUniverses
 
 import System.IO
+import System.Environment
+import System.Exit
+import Data.List
 
-repl :: IO ()
-repl = do
+readWithFlags :: [String] -> String -> Context Set
+readWithFlags flags str = let
+  input = (!! 1) <$> find ((== 'i') . head) flags
+  in case input of
+    Nothing -> parseExpr "<input>" str >>= interpretExpr
+    Just u -> case lookup u universes of
+      Nothing -> throwError $ "Unknown universe: " ++ [u]
+      Just un -> case universeRead un of
+        Nothing -> throwError $ "Cannot parse in universe: " ++ [u]
+        Just f -> f str
+
+showWithFlags :: [String] -> Set -> Context String
+showWithFlags flags set = let
+  output = (!! 1) <$> find ((== 'o') . head) flags
+  in case output of
+    Nothing -> pure $ show set
+    Just u -> case lookup u universes of
+      Nothing -> throwError $ "Unknown universe: " ++ [u]
+      Just un -> case universeShow un of
+        Nothing -> throwError $ "Cannot show in universe: " ++ [u]
+        Just f -> f set
+
+repl :: [String] -> IO ()
+repl flags = do
   putStr "> "
   hFlush stdout
   line <- getLine
   if line == ":q" then return ()
-  else runContext (run "<repl>" line) >>= putStrLn . (\case
-    Left err -> err
-    Right Nothing -> ""
-    Right (Just v) -> show v) >> repl
+  else runContext (run "<repl>" line) >>= (\case
+    Left err -> putStrLn err
+    Right Nothing -> return ()
+    Right (Just v) -> runContext (showWithFlags flags v) >>= (\case
+      Left err -> putStrLn err
+      Right str -> putStrLn str)) >> repl flags
+
+runFile :: [String] -> FilePath -> [String] -> IO ()
+runFile flags file args = do
+  inputs <- mapM (\arg -> runContext (readWithFlags flags arg) >>= (\case
+    Left err -> putStrLn err *> exitFailure
+    Right res -> pure res)) args
+  contents <- readFile file
+  let str = contents ++ " " ++ intercalate " " (show <$> inputs)
+  runContext (run file str) >>= (\case
+    Left err -> putStrLn err
+    Right Nothing -> pure ()
+    Right (Just v) -> runContext (showWithFlags flags v) >>= (\case
+      Left err -> putStrLn err *> exitFailure
+      Right r -> putStrLn r))
 
 main :: IO ()
 main = do
@@ -22,4 +67,9 @@ main = do
   hSetEncoding stdout utf8
   hSetEncoding stderr utf8
 
-  repl
+  allArgs <- getArgs
+  let posArgs = filter (not . isPrefixOf "-") allArgs
+  let flags = allArgs >>= (\a -> if null a then [] else if head a == '-' then [tail a] else [])
+
+  if null posArgs then repl flags
+  else runFile flags (head posArgs) (tail posArgs)
